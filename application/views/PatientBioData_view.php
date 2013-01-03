5ccc8e6f629ae7e2d6aa721c70a45f027c0877f0
wikee <bilal.26@gmail.com>
2013-01-04 01:24:35 +0500

    added style, widgets, script modified login_view patient_biodata_view

--- application/views/PatientBioData_view.php
+++ application/views/PatientBioData_view.php
@@ -3,45 +3,105 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
  <head>
    <title>Patient Bio Data</title>
+    <link rel="stylesheet" href="assets/jquery/jqwidgets/styles/jqx.base.css" type="text/css" />
+	<link rel="stylesheet" href="assets/style.css" type="text/css" />
+    <script type="text/javascript" src="assets/jquery/scripts/gettheme.js"></script>
+    <script type="text/javascript" src="assets/jquery/scripts/jquery-1.8.2.min.js"></script>
+    <script type="text/javascript" src="assets/jquery/jqwidgets/jqxcore.js"></script>
+    <script type="text/javascript" src="assets/jquery/jqwidgets/jqxwindow.js"></script>
+    <script type="text/javascript" src="assets/jquery/jqwidgets/jqxbuttons.js"></script>
+    <script type="text/javascript">
+        var customButtonsDemo = (function () {
+            var _collapsed = false;
+            function _createElements() {
+                $('#customWindow').jqxWindow({ theme: customButtonsDemo.config.theme, width: 400,
+                    height: 450, resizable: false,
+                    initContent: function () {
+                        $('#EnterButton').jqxButton({ theme: customButtonsDemo.config.theme, width: '80px', disabled: false });
+                    }
+                });
+            };
+            return {
+                config: {
+                    theme: null
+                },
+                init: function () {
+                    _createElements();
+					$('#customWindow').jqxWindow('draggable', false);
+                }
+            };
+        } ());
+        $(document).ready(function () {
+            var theme = $.data(document.body, 'theme', theme);
+            if (theme == undefined) theme = '';
+            customButtonsDemo.config.theme = theme;
+            customButtonsDemo.init();
+        });
+    </script>
  </head>
- <body>
-   <h1>Patient Bio Data</h1>
-   <?php //echo validation_errors(); 
-   ?>
-   <?php echo form_open('PatientBioDataController'); ?>
-     <label for="almoner_number">Almoner #:</label>
-     <input type="text" size="20" id="almoner_number" name="almoner_number"/>
-     <br/>
-     <label for="name">Name:</label>
-     <input type="text" size="50" id="name" name="name"/>
-     <br/>
-	 <label for="age">Age:</label>
-     <input type="text" size="3" id="age" name="age"/>
-     <br/>
-	 <label for="sex">Sex:</label>
-     <input type="radio" id="sex" name="sex" value="1" <?php echo set_radio('sex', '1', TRUE); ?> />Male
-	 <input type="radio" id="sex" name="sex" value="0"<?php echo set_radio('sex', '0'); ?>/>Female
-     <br/>
-	 <label for="father_name">Father Name:</label>
-     <input type="text" size="50" id="father_name" name="father_name"/>
-     <br/>
-	 <label for="occupation">Occupation:</label>
-     <input type="text" size="50" id="occupation" name="occupation"/>
-     <br/>
-	 <label for="address">Address:</label>
-     <input type="text" size="256" id="address" name="address"/>
-     <br/>
-	 <label for="telephone_number">Telephone #:</label>
-     <input type="text" size="20" id="telephone_number" name="telephone_number"/>
-     <br/>
-	 <label for="bed_number">Bed #:</label>
-     <input type="text" size="20" id="bed_number" name="bed_number"/>
-     <br/>
-	 <label for="admitted_from">Admitted From:</label>
-     <input type="text" size="20" id="admitted_from" name="admitted_from"/>
-     <br/>
-     <input type="submit" value="Submit"/>
-   </form>
+ <body class="default">
+      
+   <div style="width: 100%; height: 650px;" id="jqxWidget">
+        <div
+            id="mainDemoContainer">
+            <div id="customWindow">
+                <div id="customWindowHeader">
+                    <span id="captureContainer" style="float: left">Patient Bio Data</span>
+                </div>
+                <div id="customWindowContent" style="overflow: hidden">
+                    <div style="margin: 10px">
+						<?php echo form_open('PatientBioData'); ?>
+                        <label class="cLabel" for="almoner_number">Almoner #:</label>
+                        <input class="cInput" type="text" size="20" style="width: 175px; border: 1px solid #aaa" id="almoner_number" name="almoner_number" />
+						<br/>
+						<br/>
+						<label class="cLabel" for="name">Name:</label>
+						<input class="cInput" type="text" size="50" style="width: 175px; border: 1px solid #aaa" id="name" name="name" />
+                        <br/>
+						<br/>
+						<label class="cLabel" for="age">Age:</label>
+						<input class="cInput" type="text" size="3" style="width: 175px; border: 1px solid #aaa" id="age" name="age" />
+                        <br/>
+						<br/>
+						<label class="cLabel" for="sex">Sex:</label>
+						<input style="width:5em, float:left" type="radio" id="sex" name="sex" value="male"/>Male
+						<input style="width:5em, float:left" type="radio" id="sex" name="sex" value="female"/>Female
+                        <br/>
+						<br/>
+						<label class="cLabel" for="father_name">Father Name:</label>
+						<input class="cInput" type="text" size="50" style="width: 175px; border: 1px solid #aaa" id="father_name" name="father_name" />
+                        <br/>
+						<br/>
+						<label class="cLabel" for="occupation">Occupation:</label>
+						<input class="cInput" type="text" size="50" style="width: 175px; border: 1px solid #aaa" id="occupation" name="occupation" />
+                        <br/>
+						<br/>
+						<label class="cLabel" for="address">Address:</label>
+						<input class="cInput" type="text" size="256" style="width: 175px; border: 1px solid #aaa" id="address" name="address" />
+                        <br/>
+						<br/>
+						<label class="cLabel" for="telephone_number">Telephone #:</label>
+						<input class="cInput" type="text" size="20" style="width: 175px; border: 1px solid #aaa" id="telephone_number" name="telephone_number" />
+                        <br/>
+						<br/>
+						<label class="cLabel" for="bed_number">Bed #:</label>
+						<input class="cInput" type="text" size="20" style="width: 175px; border: 1px solid #aaa" id="bed_number" name="bed_number" />
+                        <br/>
+						<br/>
+						<label class="cLabel" for="admitted_from">Admitted From:</label>
+						<input class="cInput" type="text" size="20" style="width: 175px; border: 1px solid #aaa" id="admitted_from" name="admitted_from" />
+                        <br/><br/>
+                        <div style="float: right">
+                            <input type="submit" value="Enter" style="margin-bottom: 5px;" id="EnterButton"/><br />
+                        </div>
+                        </form>
+                </div>
+            </div>
+        </div>
+    </div>
+   
+   
  </body>
+ 
 </html>
 
--- application/views/login_view.php
+++ application/views/login_view.php
@@ -2,20 +2,73 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
  <head>
-   <title>Simple Login with CodeIgniter</title>
+   <title>Login</title>
+    <link rel="stylesheet" href="assets/jquery/jqwidgets/styles/jqx.base.css" type="text/css"/>
+	<link rel="stylesheet" href="assets/style.css" type="text/css"/>
+    <script type="text/javascript" src="assets/jquery/scripts/gettheme.js"></script>
+    <script type="text/javascript" src="assets/jquery/scripts/jquery-1.8.2.min.js"></script>
+    <script type="text/javascript" src="assets/jquery/jqwidgets/jqxcore.js"></script>
+    <script type="text/javascript" src="assets/jquery/jqwidgets/jqxwindow.js"></script>
+    <script type="text/javascript" src="assets/jquery/jqwidgets/jqxbuttons.js"></script>
+    <script type="text/javascript">
+        var customButtonsDemo = (function () {
+            var _collapsed = false;
+            function _createElements() {
+                $('#customWindow').jqxWindow({ theme: customButtonsDemo.config.theme, width: 400,
+                    height: 150, resizable: false,
+                    initContent: function () {
+                        $('#loginButton').jqxButton({ theme: customButtonsDemo.config.theme, width: '80px', disabled: false });
+                    }
+                });
+            };
+            return {
+                config: {
+                    theme: null
+                },
+                init: function () {
+                    _createElements();
+					$('#customWindow').jqxWindow('draggable', false);
+                }
+            };
+        } ());
+        $(document).ready(function () {
+            var theme = $.data(document.body, 'theme', theme);
+            if (theme == undefined) theme = '';
+            customButtonsDemo.config.theme = theme;
+            customButtonsDemo.init();
+        });
+    </script>
  </head>
- <body>
-   <h1>Simple Login with CodeIgniter</h1>
-   <?php echo validation_errors(); ?>
-   <?php echo form_open('verifylogin'); ?>
-     <label for="username">Username:</label>
-     <input type="text" size="20" id="username" name="username"/>
-     <br/>
-     <label for="password">Password:</label>
-     <input type="password" size="20" id="passowrd" name="password"/>
-     <br/>
-     <input type="submit" value="Login"/>
-   </form>
+ <body class="default">
+      
+   <div style="width: 100%; height: 650px;" id="jqxWidget">
+        <div
+            id="mainDemoContainer">
+            <div id="customWindow">
+                <div id="customWindowHeader">
+                    <span id="captureContainer" style="float: left">Login </span>
+                </div>
+                <div id="customWindowContent" style="overflow: hidden">
+                    <div style="margin: 10px">
+						<?php echo form_open('verifylogin'); ?>
+                        <label class="cLabel" for="username">Username:</label>
+                        <input class="cInput" type="text" size="20" style="width: 175px; border: 1px solid #aaa" id="username" name="username" />
+						<br/>
+						<br/>
+						<label class="cLabel" for="password">Password:</label>
+						<input class="cInput" type="password" size="20" style="width: 175px; border: 1px solid #aaa" id="password" name="password" />
+                        <br/><br/>
+                        <div style="float: right">
+                            <input type="submit" value="Login" style="margin-bottom: 5px;" id="loginButton"/><br />
+                        </div>
+                        </form>
+                </div>
+            </div>
+        </div>
+    </div>
+   
+   
  </body>
+ 
 </html>
 
